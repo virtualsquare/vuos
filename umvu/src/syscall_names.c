9fbab1ff4c86cb9f56e338ccb00d08c67cfa60b5
Renzo Davoli <renzo@cs.unibo.it>
2017-10-18 19:35:12 +0200

    license headers added

--- umvu/src/syscall_names.c
+++ umvu/src/syscall_names.c
@@ -1,3 +1,24 @@
+/*
+ *   VUOS: view OS project
+ *   Copyright (C) 2017  Renzo Davoli <renzo@cs.unibo.it>, Antonio Cardace <anto.cardace@gmail.com>
+ *   VirtualSquare team.
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *   UMDEV: Virtual Device in Userspace
+ *
+ */
+
 #define UNKNOWN_SYSTEM_CALL "unknown"
 #define DEBUG
 #if defined(DEBUG) && defined(__x86_64__)
