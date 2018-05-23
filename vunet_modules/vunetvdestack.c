b6668164d8eab04fe7ccb0c77494c3b6f65d82be
Renzo Davoli <renzo@cs.unibo.it>
2018-05-23 09:08:08 +0200

    vunetvdestack fix: clone/pthread incompatibilities
    CLONE_THREAD and CLONE_NEWUSER are incompatible but
    CLONE_THREAD seems to be required for pthread locking to work
    solution: avoid CLONE_VM

--- vunet_modules/vunetvdestack.c
+++ vunet_modules/vunetvdestack.c
@@ -33,7 +33,6 @@
 #include <net/if.h>
 #include <linux/if_tun.h>
 #include <libvdeplug.h>
-#include <r_table.h>
 
 /* just in case prctl.h is not providing these definitions */
 #ifndef PR_CAP_AMBIENT
@@ -56,45 +55,45 @@
 
 struct vdestack {
 	pid_t pid;
-  int cmdpipe[2]; // socketpair for commands;
+	int cmdpipe[2]; // socketpair for commands;
 	VDECONN *vdeconn;
-	 char *child_stack;
-  char ifname[];
+	char *child_stack;
+	char ifname[];
 };
 
 struct vdecmd {
-  int domain;
-  int type;
-  int protocol;
+	int domain;
+	int type;
+	int protocol;
 };
 
 struct vdereply {
-  int rval;
-  int err;
+	int rval;
+	int err;
 };
 
 static int open_tap(char *name) {
-  struct ifreq ifr;
-  int fd=-1;
-  if((fd = open("/dev/net/tun", O_RDWR | O_CLOEXEC)) < 0)
-    return -1;
-  memset(&ifr, 0, sizeof(ifr));
-  ifr.ifr_flags = IFF_TAP | IFF_NO_PI;
-  strncpy(ifr.ifr_name, name, sizeof(ifr.ifr_name) - 1);
-  if(ioctl(fd, TUNSETIFF, (void *) &ifr) < 0) {
-    close(fd);
-    return -1;
-  }
-  return fd;
+	struct ifreq ifr;
+	int fd=-1;
+	if((fd = open("/dev/net/tun", O_RDWR | O_CLOEXEC)) < 0)
+		return -1;
+	memset(&ifr, 0, sizeof(ifr));
+	ifr.ifr_flags = IFF_TAP | IFF_NO_PI;
+	strncpy(ifr.ifr_name, name, sizeof(ifr.ifr_name) - 1);
+	if(ioctl(fd, TUNSETIFF, (void *) &ifr) < 0) {
+		close(fd);
+		return -1;
+	}
+	return fd;
 }
 
 static int childFunc(void *arg)
 {
-  struct vdestack *stack = arg;
+	struct vdestack *stack = arg;
 	int n;
-  char buf[VDE_ETHBUFSIZE];
+	char buf[VDE_ETHBUFSIZE];
 	int tapfd = open_tap(stack->ifname);
-  VDECONN *conn = stack->vdeconn;
+	VDECONN *conn = stack->vdeconn;
 	struct pollfd pfd[] = {{stack->cmdpipe[DAEMONSIDE], POLLIN, 0},
 		{tapfd, POLLIN, 0},
 		{vde_datafd(conn), POLLIN, 0}};
@@ -102,88 +101,88 @@ static int childFunc(void *arg)
 		perror("tapfd"); _exit(1);
 	}
 	pfd[1].fd = tapfd;
-	while (r_poll(pfd, 3, -1) >= 0) {
+	while (poll(pfd, 3, -1) >= 0) {
 		//printk("poll in %d %d %d\n",pfd[0].revents,pfd[1].revents,pfd[2].revents);
 		if (pfd[0].revents & POLLIN) {
 			struct vdecmd cmd;
 			struct vdereply reply;
 			int n;
-			if ((n = r_read(stack->cmdpipe[DAEMONSIDE], &cmd, sizeof(cmd))) > 0) {
-				reply.rval = r_socket(cmd.domain, cmd.type, cmd.protocol);
+			if ((n = read(stack->cmdpipe[DAEMONSIDE], &cmd, sizeof(cmd))) > 0) {
+				reply.rval = socket(cmd.domain, cmd.type, cmd.protocol);
 				reply.err = errno;
-				r_write(stack->cmdpipe[DAEMONSIDE], &reply, sizeof(reply));
+				write(stack->cmdpipe[DAEMONSIDE], &reply, sizeof(reply));
 			} else
 				break;
 		}
 		if (pfd[1].revents & POLLIN) {
-			n = r_read(tapfd, buf, VDE_ETHBUFSIZE);
+			n = read(tapfd, buf, VDE_ETHBUFSIZE);
 			if (n == 0) break;
 			vde_send(conn, buf, n, 0);
 		}
 		if (pfd[2].revents & POLLIN) {
 			n = vde_recv(conn, buf, VDE_ETHBUFSIZE, 0);
 			if (n == 0) break;
-			r_write(tapfd, buf, n);
+			write(tapfd, buf, n);
 		}
 		//printk("poll out\n");
 	}
-  r_close(stack->cmdpipe[DAEMONSIDE]);
+	close(stack->cmdpipe[DAEMONSIDE]);
 	_exit(EXIT_SUCCESS);
 }
 
 struct vdestack *vde_addstack(char *vdenet, char *ifname) {
-	  char *ifnameok = ifname ? ifname : DEFAULT_IF_NAME;
-  size_t ifnameoklen = strlen(ifnameok);
-  struct vdestack *stack = malloc(sizeof(*stack) + ifnameoklen + 1);
+	char *ifnameok = ifname ? ifname : DEFAULT_IF_NAME;
+	size_t ifnameoklen = strlen(ifnameok);
+	struct vdestack *stack = malloc(sizeof(*stack) + ifnameoklen + 1);
 
-  if (stack) {
+	if (stack) {
 		strncpy(stack->ifname, ifnameok, ifnameoklen + 1);
 		stack->child_stack = malloc(CHILD_STACK_SIZE);
-    if (stack->child_stack == NULL)
-      goto err_child_stack;
+		if (stack->child_stack == NULL)
+			goto err_child_stack;
 
 		if (socketpair(AF_UNIX, SOCK_SEQPACKET | SOCK_CLOEXEC, 0, stack->cmdpipe) < 0)
-      goto err_cmdpipe;
+			goto err_cmdpipe;
 
 		if ((stack->vdeconn = vde_open(vdenet, "vdestack", NULL)) == NULL)
-        goto err_vdenet;
+			goto err_vdenet;
 
 		stack->pid = clone(childFunc, stack->child_stack + CHILD_STACK_SIZE,
-        CLONE_VM | CLONE_FILES | CLONE_NEWUSER | CLONE_NEWNET | SIGCHLD, stack);
-    if (stack->pid == -1)
-      goto err_child;
-    //uid_gid_map(stack->pid); //is this required?
+				CLONE_FILES | CLONE_NEWUSER | CLONE_NEWNET | SIGCHLD, stack);
+		if (stack->pid == -1)
+			goto err_child;
+		//uid_gid_map(stack->pid); //is this required?
 	}
 	return stack;
 err_child:
 err_vdenet:
-	r_close(stack->cmdpipe[APPSIDE]);
-	r_close(stack->cmdpipe[DAEMONSIDE]);
+	close(stack->cmdpipe[APPSIDE]);
+	close(stack->cmdpipe[DAEMONSIDE]);
 err_cmdpipe:
 	free(stack->child_stack);
 err_child_stack:
-  free(stack);
-  return NULL;
+	free(stack);
+	return NULL;
 }
 
 void vde_delstack(struct vdestack *stack) {
 	vde_close(stack->vdeconn);
-	r_close(stack->cmdpipe[APPSIDE]);
+	close(stack->cmdpipe[APPSIDE]);
 	waitpid(stack->pid, NULL, 0);
 	free(stack->child_stack);
-  free(stack);
+	free(stack);
 }
 
 int vde_msocket(struct vdestack *stack, int domain, int type, int protocol) {
 	struct vdecmd cmd = {domain, type, protocol};
-  struct vdereply reply;
+	struct vdereply reply;
 
-  r_write(stack->cmdpipe[APPSIDE],  &cmd, sizeof(cmd));
-  r_read(stack->cmdpipe[APPSIDE], &reply, sizeof(reply));
+	write(stack->cmdpipe[APPSIDE],  &cmd, sizeof(cmd));
+	read(stack->cmdpipe[APPSIDE], &reply, sizeof(reply));
 
-  if (reply.rval < 0)
-    errno = reply.err;
-  return reply.rval;
+	if (reply.rval < 0)
+		errno = reply.err;
+	return reply.rval;
 }
 
 static int supported_domain (int domain) {
