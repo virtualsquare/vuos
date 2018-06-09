82e7127c13ddedadfeb714f90a1e889152f7fec2
Renzo Davoli <renzo@cs.unibo.it>
2018-06-09 18:38:48 +0200

    pretty printing (indentation)

--- umvu/src/read_proc_status.c
+++ umvu/src/read_proc_status.c
@@ -30,44 +30,44 @@ void status_getresfuid(pid_t tid, uid_t *ruid, uid_t *euid, uid_t *suid, uid_t *
 	char *uidline = getlinetag(tid, "Uid");
 	if (uidline) {
 		char *s = uidline +  4;
-    *ruid = strtoul(s, &s, 0);
-    *euid = strtoul(s, &s, 0);
-    *suid = strtoul(s, &s, 0);
-    *fsuid = strtoul(s, &s, 0);
-    free(uidline);
+		*ruid = strtoul(s, &s, 0);
+		*euid = strtoul(s, &s, 0);
+		*suid = strtoul(s, &s, 0);
+		*fsuid = strtoul(s, &s, 0);
+		free(uidline);
 	} else
 		*ruid = *euid = *suid = *fsuid = (uid_t) -1;
 }
 
 void status_getresfgid(pid_t tid, gid_t *rgid, gid_t *egid, gid_t *sgid, gid_t *fsgid) {
-  char *gidline = getlinetag(tid, "Gid");
-  if (gidline) {
+	char *gidline = getlinetag(tid, "Gid");
+	if (gidline) {
 		char *s = gidline +  4;
-    *rgid = strtoul(s, &s, 0);
-    *egid = strtoul(s, &s, 0);
-    *sgid = strtoul(s, &s, 0);
-    *fsgid = strtoul(s, &s, 0);
-    free(gidline);
-  } else
-    *rgid = *egid = *sgid = *fsgid = (gid_t) -1;
+		*rgid = strtoul(s, &s, 0);
+		*egid = strtoul(s, &s, 0);
+		*sgid = strtoul(s, &s, 0);
+		*fsgid = strtoul(s, &s, 0);
+		free(gidline);
+	} else
+		*rgid = *egid = *sgid = *fsgid = (gid_t) -1;
 }
 
 int status_getgroups(pid_t tid, int size, gid_t list[]) {
 	char *groupsline = getlinetag(tid, "Groups");
 	if (groupsline) {
-		    char *s;
-    int n;
-    for (s = groupsline + 7, n = 0; *s != '\0'; n++, s += strspn(s, " \t\n")) {
-      unsigned long g = strtoul(s, &s, 0);
-      if (size != 0) {
-        if (n < size)
-          list[n] = g;
-        else
-          break;
-      }
-    }
-    free(groupsline);
-    return (*s == '\0') ? n : -1;
+		char *s;
+		int n;
+		for (s = groupsline + 7, n = 0; *s != '\0'; n++, s += strspn(s, " \t\n")) {
+			unsigned long g = strtoul(s, &s, 0);
+			if (size != 0) {
+				if (n < size)
+					list[n] = g;
+				else
+					break;
+			}
+		}
+		free(groupsline);
+		return (*s == '\0') ? n : -1;
 	}
 	else
 		return -1;
