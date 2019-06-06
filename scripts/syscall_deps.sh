d86f168acd9d7ea66fc34d39904b36d4684a72b7
Renzo Davoli <renzo@cs.unibo.it>
2019-06-06 16:18:02 +0200

    new infrastructure for man pages based on ronn
    	+ man page for umvu(1) (to be completed)

--- scripts/syscall_deps.sh
+++ scripts/syscall_deps.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# compute and prin on stdout all the (nested_ dependencies of "#include<sys/syscall.h>"
+
 echo "#include<sys/syscall.h>" | gcc -M -E - | \
 			 python3 -c 'import sys,re; print (";".join([re.sub("^(-:  | )","",re.sub(" \\\$","",line[:-1])) for line in sys.stdin]))'
 
