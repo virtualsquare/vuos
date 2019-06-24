virtualsquare/vuos

commit b17777c55054dc9ff2f6ef187654e162e971c06b

new signature of vu_wrap_rw_multiplex functions

umvu/src/vu_wrap_rw_multiplex.c
@@ -41,28 +41,44 @@ static void wi_einval(struct vuht_entry_t *ht, struct syscall_descriptor_t *sd) 
 	}
 }
 
-void set_wi_read(mode_t mode, wrapf_t *handler) {
-	x_wi_read[S_MODE2TYPE(mode)] = handler;
+void multiplex_read_wrappers(mode_t mode,
+    wrapf_t wrapin, wrapf_t wrapduring, wrapf_t wrapout) {
+	int modeindex = S_MODE2TYPE(mode);
+
+	if (wrapin == NULL)
+		x_wi_read[modeindex] = wi_einval;
+	else
+		x_wi_read[modeindex] = wrapin;
+
+	if (wrapduring == NULL)
+		x_wd_read[modeindex] = wd_NULL;
+	else
+		x_wd_read[modeindex] = wrapduring;
+
+	if (wrapout == NULL)
+		x_wo_read[modeindex] = wo_NULL;
+	else
+		x_wo_read[modeindex] = wrapduring;
 }
 
-void set_wd_read(mode_t mode, wrapf_t *handler) {
-	x_wd_read[S_MODE2TYPE(mode)] = handler;
-}
+void multiplex_write_wrappers(mode_t mode,
+    wrapf_t wrapin, wrapf_t wrapduring, wrapf_t wrapout) {
+	int modeindex = S_MODE2TYPE(mode);
 
-void set_wo_read(mode_t mode, wrapf_t *handler) {
-	x_wo_read[S_MODE2TYPE(mode)] = handler;
-}
+	if (wrapin == NULL)
+		x_wi_write[modeindex] = wi_einval;
+	else
+		x_wi_write[modeindex] = wrapin;
 
-void set_wi_write(mode_t mode, wrapf_t *handler) {
-	x_wi_write[S_MODE2TYPE(mode)] = handler;
-}
+	if (wrapduring == NULL)
+		x_wd_write[modeindex] = wd_NULL;
+	else
+		x_wd_write[modeindex] = wrapduring;
 
-void set_wd_write(mode_t mode, wrapf_t *handler) {
-	x_wd_write[S_MODE2TYPE(mode)] = handler;
-}
-
-void set_wo_write(mode_t mode, wrapf_t *handler) {
-	x_wo_write[S_MODE2TYPE(mode)] = handler;
+	if (wrapout == NULL)
+		x_wo_write[modeindex] = wo_NULL;
+	else
+		x_wo_write[modeindex] = wrapduring;
 }
 
 void wi_read(struct vuht_entry_t *ht, struct syscall_descriptor_t *sd) {

umvu/include/vu_wrap_rw_multiplex.h
@@ -3,11 +3,19 @@
 #include <sys/stat.h>
 #include <syscall_table.h>
 
-void set_wi_read(mode_t mode, wrapf_t *handler);
-void set_wd_read(mode_t mode, wrapf_t *handler);
-void set_wo_read(mode_t mode, wrapf_t *handler);
-void set_wi_write(mode_t mode, wrapf_t *handler);
-void set_wd_write(mode_t mode, wrapf_t *handler);
-void set_wo_write(mode_t mode, wrapf_t *handler);
+/* this hypervisor module defines the wrappers for read and write and
+	 dispatch the requests to specific wrappersdepending on the file types.
+
+	 e.g. read and write have different implementations when operating
+	 on regular files, block or char devices.
+	 read/write are mapped to recvmsg/sendmsg if the file is a socket.
+
+	 The following functions permits the definition of specific handlers
+	 per file type */
+
+void multiplex_read_wrappers(mode_t mode,
+		wrapf_t wrapin, wrapf_t wrapduring, wrapf_t wrapout);
+void multiplex_write_wrappers(mode_t mode,
+		wrapf_t wrapin, wrapf_t wrapduring, wrapf_t wrapout);
 
 #endif

umvu/src/vu_wrap_file.c
@@ -675,14 +675,10 @@ __attribute__((constructor))
 		vu_fnode_set_close_upcall(S_IFCHR, file_close_upcall);
 		vu_fnode_set_close_upcall(S_IFBLK, file_close_upcall);
 		vu_fnode_set_close_upcall(S_IFLNK, file_close_upcall);
-		set_wi_read(S_IFREG, file_wi_read);
-		set_wi_read(S_IFBLK, file_wi_read);
-		set_wi_write(S_IFREG, file_wi_write);
-		set_wi_write(S_IFBLK, file_wi_write);
-		set_wi_read(S_IFCHR, slow_wi_read);
-		set_wd_read(S_IFCHR, slow_wd_read);
-		set_wo_read(S_IFCHR, slow_wo_read);
-		set_wi_write(S_IFCHR, slow_wi_write);
-		set_wd_write(S_IFCHR, slow_wd_write);
-		set_wo_write(S_IFCHR, slow_wo_write);
+		multiplex_read_wrappers(S_IFREG, file_wi_read, NULL, NULL);
+		multiplex_read_wrappers(S_IFBLK, file_wi_read, NULL, NULL);
+		multiplex_read_wrappers(S_IFCHR, slow_wi_read, slow_wd_read, slow_wo_read);
+		multiplex_write_wrappers(S_IFREG, file_wi_write, NULL, NULL);
+		multiplex_write_wrappers(S_IFBLK, file_wi_write, NULL, NULL);
+		multiplex_write_wrappers(S_IFCHR, slow_wi_write, slow_wd_write, slow_wo_write);
 	}

umvu/src/vu_wrap_socket.c
@@ -703,10 +703,6 @@ void wi_getsockopt(struct vuht_entry_t *ht, struct syscall_descriptor_t *sd) {
 __attribute__((constructor))
 	static void init(void) {
 		vu_fnode_set_close_upcall(S_IFSOCK, socket_close_upcall);
-		set_wi_read(S_IFSOCK, wi_recvfrom);
-		set_wd_read(S_IFSOCK, wd_recvfrom);
-		set_wo_read(S_IFSOCK, wo_recvfrom);
-		set_wi_write(S_IFSOCK, wi_sendto);
-		set_wd_write(S_IFSOCK, wd_sendto);
-		set_wo_write(S_IFSOCK, wo_sendto);
+		multiplex_read_wrappers(S_IFSOCK, wi_recvfrom, wd_recvfrom, wo_recvfrom);
+		multiplex_write_wrappers(S_IFSOCK, wi_sendto, wd_sendto, wo_sendto);
 	}

commit 2d8f89357b79fb745dd67cec80d0db4352992144

Bugfix: ivu_wrap_rw_multiplex wrong wrapout.
  the bug was introduced in b17777c

umvu/src/vu_wrap_rw_multiplex.c
@@ -58,7 +58,7 @@ void multiplex_read_wrappers(mode_t mode,
 	if (wrapout == NULL)
 		x_wo_read[modeindex] = wo_NULL;
 	else
-		x_wo_read[modeindex] = wrapduring;
+		x_wo_read[modeindex] = wrapout;
 }
 
 void multiplex_write_wrappers(mode_t mode,
@@ -78,7 +78,7 @@ void multiplex_write_wrappers(mode_t mode,
 	if (wrapout == NULL)
 		x_wo_write[modeindex] = wo_NULL;
 	else
-		x_wo_write[modeindex] = wrapduring;
+		x_wo_write[modeindex] = wrapout;
 }
 
 void wi_read(struct vuht_entry_t *ht, struct syscall_descriptor_t *sd) {